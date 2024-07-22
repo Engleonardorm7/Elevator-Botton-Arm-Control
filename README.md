b73e522000d9981427c8f108c6cfd51ca580e70f
Leonardo <leonardorm7@hotmail.com>
2024-07-22 17:23:49 +0200

    README edited

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# Elevator-Botton-Arm-Control
+# Elevator-Button-Arm-Control
 
-## "yolov5" folder contains all codes and datasets for button detection and "Pressed_NonPressed_model" folder contains all codes and datasets for pressed unpressed button detection
+This project controls the Niryo Ned2 robotic arm using a YOLO image detection model to detect and press elevator buttons.
 
-### To run this code successfully with Niryo Arm:
+## Project Description
+
+The main file of the project is `project.py`, which contains the general code. The process is as follows:
+
+1. Connect to the robot.
+2. The robot moves to the initial position, waits for 2 seconds, and takes a picture.
+3. The system asks the user which floor they want to go to. Based on the input number, the robot sends this information to the YOLO model.
+4. The YOLO model returns the coordinates of the desired button.
+5. If the button is already pressed, it will display the message "Button is already pressed".
+6. If the button is not pressed, the YOLO model will send the coordinates of the desired button.
+7. Using inverse kinematics, the angles for each joint of the robot are calculated to reach the button's position.
+8. Once the robot is in front of the button, it will move forward, wait for one second, and move backward to press the button.
+9. After this, the robot waits for 5 seconds and then goes to sleep mode.
+
+## Directory Structure
+
+- The `yolov5` folder contains all codes and datasets for button detection.
+- The `Pressed_NonPressed_model` folder contains all codes and datasets for pressed/unpressed button detection.
+
+## Running the Code
+
+### With Niryo Arm
 
 1. Activate virtual environment:
 ```
@@ -22,21 +43,27 @@ niryorobot
 python project.py 
 ```
 
-### To run this code successfully without Niryo Arm:
+### Without Niryo Arm
 
 
 1. Activate virtual environment:
 ```
 source robotics/bin/activate
 ```
-or create a new virtual environment and install "requirements.txt" using the following command and activate that environment:
+or create a new virtual environment, install the dependencies from `requirements.txt`, and activate that environment:
 ```
 pip install -r /path/to/requirements.txt
 ```
-2. Change image_path from main.py. There are some test images in the "test_image" folder.
+2. Change the `image_path` in `main.py`. There are some test images in the `test_image` folder.
    
 3.  Run the following command from the activated virtual environment
 
 ```
 python main.py 
 ```
+## Video Demonstration
+
+<video width="600" controls>
+  <source src="Videos/Robotic Arm.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
--- project.py
+++ project.py
@@ -7,42 +7,34 @@ from button_detection import ocr_on_bounding_boxes
 
 
 ned = NiryoRobot("10.10.10.10")
-# ned = NiryoRobot("169.254.114.65")
-# ned = NiryoRobot("169.254.200.200")
+
 initial_pos=ned.get_pose()
-print(initial_pos)
+
 print(ned.need_calibration())
 ned.calibrate_auto()
 ned.move_to_home_pose()
 time.sleep(3)
-# ned.tool_reboot()
-# ned.reset_tcp()
+
 
 #*******************Starting position*************************
+
 initial_pos=(0.1353,-0.0089, 0.2372,-0.056,0.080, -0.063)
-# initial_pos=(0.2851,-0.0012, 0.05,0.019,0.073,-0.003)
-ned.move_pose(initial_pos)
 
-# ned.move_joints(-2.9, -0.3, 0.1, 0.0, 0.5, -0.8)
-time.sleep(2)
-# ned.go_to_sleep()
+ned.move_pose(initial_pos)#The arm goes to the initial position 
 
 
-# ned.move_joints(-2.9, -0.3, 0.1, 0.0, 0.5, -0.8)#Joints are expressed in radians
-# ned.move_joints(1.0, -0.6, -1.0, 0.0, 0.5, -0.8)
-# ned.move_joints([1.3071506023406982, 0.16242043673992157, -0.652149498462677, -1.6970915794372559, -1.3387484550476074, 2.0752642154693604])
+time.sleep(2)#The arm waits 2 seconds
 
-# ned.move_pose(x=200, y=200, z=200, roll=0, pitch=0, yaw=0)
+
 
 #*******************take the picture*************************
 
 
-take_photos()
+take_photos()# The robot takes the photo
 
 
 
-#*******************receive position*************************
-
+#*******************Generate the coordinates of the button*************************
 
 image_path = '/media/asmany/Drive_D/Intelligent_Robotics/cameraphoto.jpg'  # Replace with the path to your image
 
@@ -50,10 +42,9 @@ image_path = '/media/asmany/Drive_D/Intelligent_Robotics/cameraphoto.jpg'  # Rep
 print('Which floor do you want to go?')
 floor_number = input()
 time.sleep(1)
-1# Perform OCR on the bounding boxes in the image
+# Perform OCR on the bounding boxes in the image
 coordinates = ocr_on_bounding_boxes(image_path, floor_number)
 
-# print(coordinates)
 
 if coordinates == None:
     print('Button is already pressed')
@@ -71,10 +62,8 @@ else:
 
 
 
-    #*******************Inverse kinematics*************************
-    #x = back-front
-    #y=left to right 
-    #z=up-down
+#*******************Inverse kinematics*************************
+
     x = 0.3067
     y = 0.1711 
     z = 0.4469
@@ -84,21 +73,15 @@ else:
 
     desired_pose = (0.3, y -desired_x*2.6, z-desired_y*2, 0, 0, 0)  # X, Y, Z, roll, pitch, yaw
 
-    # desired_pose = (x+desired_x, y-desired_y, 0.1, roll, pitch, yaw)  # X, Y, Z, roll, pitch, yaw
-
-    # desired_pose = (x, y, z, roll, pitch, yaw)  # X, Y, Z, roll, pitch, yaw
     joint_angles = ned.inverse_kinematics(desired_pose)
-    # print(joint_angles)
+
     ned.move_joints(joint_angles)
 
 
-    #*******************press the button*************************
+#*******************press the button*************************
 
-    # x2,y2,z2,roll2,pitch2,yaw2 = desired_pose
-    # press=(x2+0.01, y2, z2, roll, pitch, yaw)
     press=(0.3+0.02, y -desired_x*2.6, z-desired_y*2, 0, 0, 0)
     joint_angles=ned.inverse_kinematics(press)
-    # print(joint_angles)
     ned.move_joints(joint_angles)
 
     time.sleep(1)
@@ -114,10 +97,6 @@ else:
 
 
 # def inverse_kinematics(x, y, z):
-    
-    
-
-
 
 
 #     return theta1, theta2, theta3, theta4, theta5, theta6
